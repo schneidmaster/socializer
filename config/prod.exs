8c10f4fe5284d8a7acccb3f4579972763a9e5a3d
Zach Schneider <zach@schneider.dev>
2019-04-16 19:40:41 -0400

    Use DATABASE_URL in production

--- config/prod.exs
+++ config/prod.exs
@@ -17,6 +17,13 @@ config :socializer, SocializerWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :socializer, Socializer.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: {:system, "DATABASE_URL"},
+  database: "",
+  ssl: true,
+  pool_size: 2
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
