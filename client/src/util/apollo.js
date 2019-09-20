dde9096bbcc3730c7f00a04bbff4333634e38689
Adam Heath <adam-h@users.noreply.github.com>
2019-09-20 10:24:02 +0800

    Add long poll fallback when WebSocket fails (#3)
    * Add long poll fallback when WebSocket fails
    
    * fix: linting

--- client/src/util/apollo.js
+++ client/src/util/apollo.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import * as AbsintheSocket from "@absinthe/socket";
 import { createAbsintheSocketLink } from "@absinthe/socket-apollo-link";
-import { Socket as PhoenixSocket } from "phoenix";
+import { Socket as PhoenixSocket, LongPoll } from "phoenix";
 import { createHttpLink } from "apollo-link-http";
 import { hasSubscription } from "@jumpn/utils-graphql";
 import { split } from "apollo-link";
@@ -19,22 +19,37 @@ const WS_URI =
     ? "wss://brisk-hospitable-indianelephant.gigalixirapp.com/socket"
     : "ws://localhost:4000/socket";
 
+function createPhoenixSocket() {
+  // Socket with full fallback to LongPoll
+  // via https://elixirforum.com/t/fall-back-to-longpoll-when-websocket-fails/23894
+  const socket = new PhoenixSocket(WS_URI, {
+    params: () => {
+      if (Cookies.get("token")) {
+        return { token: Cookies.get("token") };
+      } else {
+        return {};
+      }
+    },
+  });
+  socket.onError(() => {
+    if (navigator.onLine) {
+      const isWebSocket = socket.transport === window.WebSocket;
+      console.error(
+        `Error connecting using ${isWebSocket ? "WebSocket" : "LongPoll"}`,
+      );
+      if (isWebSocket) socket.transport = LongPoll;
+      else if (window.WebSocket) socket.transport = window.WebSocket;
+    }
+  });
+  return socket;
+}
+
 export const createClient = ({ ssr, req, fetch, tokenCookie } = {}) => {
   let link = createHttpLink({ uri: HTTP_URI, fetch });
   let absintheSocket;
 
   if (!ssr) {
-    absintheSocket = AbsintheSocket.create(
-      new PhoenixSocket(WS_URI, {
-        params: () => {
-          if (Cookies.get("token")) {
-            return { token: Cookies.get("token") };
-          } else {
-            return {};
-          }
-        },
-      }),
-    );
+    absintheSocket = AbsintheSocket.create(createPhoenixSocket());
     const socketLink = createAbsintheSocketLink(absintheSocket);
 
     link = split(
